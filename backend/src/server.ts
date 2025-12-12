82e142e00be28048752a2f579e93a7f584626f92
kevin <kevin.ornik@student.um.si>
2025-12-12 20:18:00 +0100

    Dodana funkcionalnost avtomatizacije dokumentacije backend enpointov

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,11 @@ import express from 'express';
 import path from 'path';
 import router from './routes';
 
+// TSOA + SWAGGER 
+import { RegisterRoutes }from "./tsoa-routes/routes";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger/swagger.json";
+
 const app = express();
 const PORT = 3000;
 
@@ -9,11 +14,18 @@ const PORT = 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(process.cwd(), 'views'));
 
+// Body parser (tsoa ga potrebuje)
+app.use(express.json());
+
 // Static (React build gre sem)
 app.use(express.static(path.join(process.cwd(), 'public')));
 
 // API routes
 app.use(router);
+// TSOA routes
+RegisterRoutes(router);
+// Swagger UI
+app.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Page route
 app.get('/', (req, res) => {
