ceec8631f0bed808d4dea15fcd269e890825f42b
AnzeBlaBla <anzemajblabla@gmail.com>
2025-12-14 11:21:14 +0100

    Dodajanje joi packaga + odstranjevanje starih routov

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,10 @@
-import express from 'express';
-import path from 'path';
-import router from './routes';
+import express from "express";
+import path from "path";
+import { Router } from "express";
 
-// TSOA + SWAGGER 
-import { RegisterRoutes }from "./tsoa-routes/routes";
+const router = Router();
+// TSOA + SWAGGER
+import { RegisterRoutes } from "./tsoa-routes/routes";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./swagger/swagger.json";
 
@@ -11,25 +12,25 @@ const app = express();
 const PORT = 3000;
 
 // Set template engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(process.cwd(), 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(process.cwd(), "views"));
 
 // Body parser (tsoa ga potrebuje)
 app.use(express.json());
 
 // Static (React build gre sem)
-app.use(express.static(path.join(process.cwd(), 'public')));
+app.use(express.static(path.join(process.cwd(), "public")));
 
 // API routes
 app.use(router);
 // TSOA routes
 RegisterRoutes(router);
 // Swagger UI
-app.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Page route
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Express + React' });
+app.get("/", (req, res) => {
+  res.render("index", { title: "Express + React" });
 });
 
 app.listen(PORT, () => {
