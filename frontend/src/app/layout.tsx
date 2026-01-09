25a640033bf811e405731ad67d3ee0f96f85b744
Sandokan <sandro.ceh@gmail.com>
2026-01-09 23:40:08 +0100

    Popravek Layouta
    Popravil sem Layout, tako da sem ohranil obstoječe vrstice in dodal še stran za urnik.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,12 +1,13 @@
-import { Outlet } from "react-router-dom"
-import Navbar from "@/components/site/navbar"
+import { Link, Outlet } from "react-router-dom"
 
 export default function RootLayout() {
   return (
     <div>
       <nav>
         <Link to="/">Home</Link>{" "}
-        <Link to="/about">About</Link>
+        <Link to="/about">About</Link>{" "}
+        <Link to="/notes">Zapiski</Link>
+        <Link to="/timetable">Urnik</Link>
       </nav>
       <Outlet />
     </div>
